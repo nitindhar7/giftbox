d7c9ef2aa7290b71313e69c3d977b7ed949023f0
Nitin Dhar <nitin@hostcommittee.com>
2015-06-15 23:29:59 -0400

    updated tabs to spaces

--- test/giftbox.js
+++ test/giftbox.js
@@ -1,7 +1,7 @@
 var Giftbox = require('../lib/giftbox'),
-	Option = Giftbox.Option,
-	Some = Giftbox.Some,
-	None = Giftbox.None;
+  Option = Giftbox.Option,
+  Some = Giftbox.Some,
+  None = Giftbox.None;
 
 exports.optionConstructor = function(test) {
     test.equal(Option('') instanceof Option, true);
@@ -15,10 +15,10 @@ exports.optionConstructor = function(test) {
     test.equal(Option([]) instanceof Option, true);
     test.equal(Option({}) instanceof Option, true);
     test.equal(Option(function() {
-    	return 'hello world';
+      return 'hello world';
     }) instanceof Option, true);
     test.strictEqual(Option(function() {
-    	return 'hello world';
+      return 'hello world';
     }).get(), 'hello world');
     test.done();
 };
@@ -56,124 +56,124 @@ exports.optionGetOrElse = function(test) {
 };
 
 exports.optionOrElse = function(test) {
-	var elseOption = Option('hello');
-	test.strictEqual(Option(null).orElse(elseOption), elseOption);
-	var option = Option(123);
-	test.strictEqual(option.orElse(elseOption), option);
-	test.done();
+  var elseOption = Option('hello');
+  test.strictEqual(Option(null).orElse(elseOption), elseOption);
+  var option = Option(123);
+  test.strictEqual(option.orElse(elseOption), option);
+  test.done();
 };
 
 exports.optionOrNull = function(test) {
-	test.strictEqual(Option(null).orNull(), null);
-	test.strictEqual(Option('hello').orNull(), 'hello');
-	test.done();
+  test.strictEqual(Option(null).orNull(), null);
+  test.strictEqual(Option('hello').orNull(), 'hello');
+  test.done();
 };
 
 exports.optionMap = function(test) {
-	test.equal(Option(null).map(function(val) {
-		return val.toUpperCase();
-	}) instanceof None, true);
-	test.equal(Option('hello').map(function(val) {
-		return val.toUpperCase();
-	}) instanceof Some, true);
-	test.strictEqual(Option('hello').map(function(val) {
-		return val.toUpperCase();
-	}).get(), 'HELLO');
-	test.done();
+  test.equal(Option(null).map(function(val) {
+    return val.toUpperCase();
+  }) instanceof None, true);
+  test.equal(Option('hello').map(function(val) {
+    return val.toUpperCase();
+  }) instanceof Some, true);
+  test.strictEqual(Option('hello').map(function(val) {
+    return val.toUpperCase();
+  }).get(), 'HELLO');
+  test.done();
 };
 
 exports.optionFlatMap = function(test) {
-	test.equal(Option('hello').flatMap(function(val) {
-		return Option(val);
-	}) instanceof Option, true);
-	test.strictEqual(Option('hello').flatMap(function(val) {
-		return Option(val.toUpperCase());
-	}).get(), 'HELLO');
-	test.strictEqual(Option('hello').flatMap(function(val) {
-		return Option(val.toUpperCase()).flatMap(function(innerval) {
-			return Option(innerval + ' WORLD!');
-		});
-	}).get(), 'HELLO WORLD!');
-	test.equal(Option(null).flatMap(function(val) {
-		return Option(val);
-	}) instanceof None, true);
-	test.done();
+  test.equal(Option('hello').flatMap(function(val) {
+    return Option(val);
+  }) instanceof Option, true);
+  test.strictEqual(Option('hello').flatMap(function(val) {
+    return Option(val.toUpperCase());
+  }).get(), 'HELLO');
+  test.strictEqual(Option('hello').flatMap(function(val) {
+    return Option(val.toUpperCase()).flatMap(function(innerval) {
+      return Option(innerval + ' WORLD!');
+    });
+  }).get(), 'HELLO WORLD!');
+  test.equal(Option(null).flatMap(function(val) {
+    return Option(val);
+  }) instanceof None, true);
+  test.done();
 };
 
 exports.optionFilter = function(test) {
-	var opt = Option('hello');
+  var opt = Option('hello');
     test.equal(opt.filter(function(val) {
-    	return val === 'hello';
+      return val === 'hello';
     }) instanceof Some, true);
     test.strictEqual(opt.get(), 'hello');
     var absent = Option(null);
     test.equal(absent.filter(function(val) {
-    	return val === 'hello';
+      return val === 'hello';
     }) instanceof None, true);
     test.strictEqual(absent.getOrElse('world'), 'world');
     test.done();
 };
 
 exports.optionFilterNot = function(test) {
-	var opt = Option('hello');
+  var opt = Option('hello');
     test.equal(opt.filterNot(function(val) {
-    	return val === 'hello';
+      return val === 'hello';
     }) instanceof None, true);
     test.strictEqual(opt.filterNot(function(val) {
-    	return val === 'hello';
+      return val === 'hello';
     }).get(), undefined);
     var absent = Option(null);
     test.equal(absent.filter(function(val) {
-    	return val === 'hello';
+      return val === 'hello';
     }) instanceof None, true);
     test.strictEqual(absent.getOrElse('world'), 'world');
     test.done();
 };
 
 exports.optionForeach = function(test) {
-	var withPrefix = ' world';
-	Option('hello').foreach(function(val) {
-		withPrefix = val + withPrefix;
-	});
-	test.strictEqual(withPrefix, 'hello world');
-	Option(null).foreach(function(val) {
-		withPrefix = val + withPrefix;
-	});
-	test.strictEqual(withPrefix, 'hello world');
+  var withPrefix = ' world';
+  Option('hello').foreach(function(val) {
+    withPrefix = val + withPrefix;
+  });
+  test.strictEqual(withPrefix, 'hello world');
+  Option(null).foreach(function(val) {
+    withPrefix = val + withPrefix;
+  });
+  test.strictEqual(withPrefix, 'hello world');
     test.done();
 };
 
 exports.optionMatch = function(test) {
-	Option('hello').match(function(val) {
-		test.strictEqual(val, 'hello');
-	}, function() {
-		// should never run. if it does, it rightly failed!
-		test.equal(true, false);
-	});
-	Option(undefined).match(function(val) {
-		// should never run. if it does, it rightly failed!
-		test.equal(true, false);
-	}, function() {
-		test.equal(true, true);
-	});
-	test.done();
+  Option('hello').match(function(val) {
+    test.strictEqual(val, 'hello');
+  }, function() {
+    // should never run. if it does, it rightly failed!
+    test.equal(true, false);
+  });
+  Option(undefined).match(function(val) {
+    // should never run. if it does, it rightly failed!
+    test.equal(true, false);
+  }, function() {
+    test.equal(true, true);
+  });
+  test.done();
 };
 
 exports.optionCollect = function(test) {
-	test.strictEqual(Option('hello').collect(function(val) {
-		return val === 'hello';
-	}, function(val) {
-		return val.toUpperCase();
-	}).get(), 'HELLO');
-	test.strictEqual(Option('world').collect(function(val) {
-		return val === 'hello';
-	}, function(val) {
-		return val.toUpperCase();
-	}) instanceof None, true);
-	test.strictEqual(Option(null).collect(function(val) {
-		return val === 'hello';
-	}, function(val) {
-		return val.toUpperCase();
-	}) instanceof None, true);
-	test.done();
+  test.strictEqual(Option('hello').collect(function(val) {
+    return val === 'hello';
+  }, function(val) {
+    return val.toUpperCase();
+  }).get(), 'HELLO');
+  test.strictEqual(Option('world').collect(function(val) {
+    return val === 'hello';
+  }, function(val) {
+    return val.toUpperCase();
+  }) instanceof None, true);
+  test.strictEqual(Option(null).collect(function(val) {
+    return val === 'hello';
+  }, function(val) {
+    return val.toUpperCase();
+  }) instanceof None, true);
+  test.done();
 };
--- test/util.js
+++ test/util.js
@@ -1,25 +1,25 @@
 var util = require('../lib/util');
 
 exports.isNaNStrict = function(test) {
-	test.strictEqual(util.isNaNStrict(1), false);
-	test.strictEqual(util.isNaNStrict('hello'), false);
-	test.strictEqual(util.isNaNStrict(undefined), false);
-	test.strictEqual(util.isNaNStrict(null), false);
-	test.strictEqual(util.isNaNStrict([]), false);
-	test.strictEqual(util.isNaNStrict({}), false);
-	test.strictEqual(util.isNaNStrict(false), false);
-	test.strictEqual(util.isNaNStrict(1/'hello'), true);
-	test.done()
+  test.strictEqual(util.isNaNStrict(1), false);
+  test.strictEqual(util.isNaNStrict('hello'), false);
+  test.strictEqual(util.isNaNStrict(undefined), false);
+  test.strictEqual(util.isNaNStrict(null), false);
+  test.strictEqual(util.isNaNStrict([]), false);
+  test.strictEqual(util.isNaNStrict({}), false);
+  test.strictEqual(util.isNaNStrict(false), false);
+  test.strictEqual(util.isNaNStrict(1/'hello'), true);
+  test.done()
 };
 
 exports.isEmpty = function(test) {
-	test.strictEqual(util.isEmpty(null), true);
-	test.strictEqual(util.isEmpty(), true);
-	test.strictEqual(util.isEmpty(1/'hello'), true);
-	test.strictEqual(util.isEmpty('hello'), false);
-	test.strictEqual(util.isEmpty(1), false);
-	test.strictEqual(util.isEmpty([]), false);
-	test.strictEqual(util.isEmpty({}), false);
-	test.strictEqual(util.isEmpty(false), false);
-	test.done()
+  test.strictEqual(util.isEmpty(null), true);
+  test.strictEqual(util.isEmpty(), true);
+  test.strictEqual(util.isEmpty(1/'hello'), true);
+  test.strictEqual(util.isEmpty('hello'), false);
+  test.strictEqual(util.isEmpty(1), false);
+  test.strictEqual(util.isEmpty([]), false);
+  test.strictEqual(util.isEmpty({}), false);
+  test.strictEqual(util.isEmpty(false), false);
+  test.done()
 };
